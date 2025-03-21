d146f1cc267c8f526a6387a6435cd85a98b2c7ca
Melksedeque <freelancer@melksedeque.com.br>
2025-03-20 22:08:21 -0300

    Criação de useRef para Labels e Inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
+import { useRef } from "react";
 import "./App.css";
 
 function App() {
+  const labelNome = useRef("");
+  const inputNome = useRef("");
+  const labelEmail = useRef("");
+  const inputEmail = useRef("");
+
   return (
     <>
       <form className="formulario">
-        <label>
+        <h2>Formulário</h2>
+        <p>Preencha os campos abaixo:</p>
+        <label ref={labelNome}>
           Nome:
-          <input type="text" name="nome" />
+          <input
+            type="text"
+            name="nome"
+            ref={inputNome}
+            placeholder="Digite seu nome"
+          />
+        </label>
+        <label ref={labelEmail}>
+          E-mail:
+          <input
+            type="email"
+            name="email"
+            ref={inputEmail}
+            placeholder="Digite seu e-mail"
+          />
         </label>
         <button type="submit">Salvar</button>
       </form>
